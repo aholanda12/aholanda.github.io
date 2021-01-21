29f17ad51a947292b41fd5da49c29071666377ac
Ashley Holanda <aholanda12@gmail.com>
2021-01-21 15:39:50 -0500

    Updates

---
+++ precache-manifest.20daaf1a8f8fed9ebf9678ab0895bc66.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3453e9e7f330175fa2eca3e3dab3c6d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f013cf2794686916186",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "adaa79b8aae3f6a73967",
+    "url": "/static/css/main.0bc7e69a.chunk.css"
+  },
+  {
+    "revision": "5f013cf2794686916186",
+    "url": "/static/js/2.74f128f0.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.74f128f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adaa79b8aae3f6a73967",
+    "url": "/static/js/main.c84f152e.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "76671af35fbe5152a72d16a11abe57ac",
+    "url": "/static/media/clearsky.76671af3.png"
+  },
+  {
+    "revision": "994c0b2f9592eed3fb9de9dfc803b692",
+    "url": "/static/media/cloud.994c0b2f.png"
+  },
+  {
+    "revision": "d163b5992c343cf573f9f080ac8cb558",
+    "url": "/static/media/headshot.d163b599.png"
+  },
+  {
+    "revision": "a9b12bf7114c67b706d8273dc4eb216a",
+    "url": "/static/media/printer.a9b12bf7.gif"
+  }
+]);
